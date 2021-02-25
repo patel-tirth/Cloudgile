987435a1928ce28cbf4899c8cb414a8473fd7bc1
Akshant Jain <akshant.jain2@gmail.com>
2021-02-24 22:17:52 -0600

    profile and settings components blank

--- Code/cloudgile/src/App.js
+++ Code/cloudgile/src/App.js
@@ -16,6 +16,8 @@ import {
 import 'firebase/auth'
 import 'firebase/firestore'
 import Dashboard from './components/Dashboard';
+import { Profile } from './components/Profile';
+import { Settings } from './components/Settings';
 
 function App() {
 
@@ -30,11 +32,10 @@ function App() {
         <Route path="/confirmation/failure"></Route>
         <ProtectedRoute path="/reminders"></ProtectedRoute>
         <ProtectedRoute path="/" exact isLoading={isLoading} isAuthed={!!user}><Dashboard/></ProtectedRoute>
-        <ProtectedRoute path="/profile" isLoading={isLoading} isAuthed={!!user}></ProtectedRoute>
-        {/* <ProtectedRoute path="/settings" isLoading={isLoading} isAuthed={!!user}></ProtectedRoute> */}
+        <ProtectedRoute path="/profile" isLoading={isLoading} isAuthed={!!user}><Profile/></ProtectedRoute>
         {/* <ProtectedRoute path="/projects/projectID/members" isLoading={isLoading} isAuthed={!!user}></ProtectedRoute> */}
         {/* <ProtectedRoute path="/projects" isLoading={isLoading} isAuthed={!!user}></ProtectedRoute> */}
-        <ProtectedRoute path="/settings" isLoading={isLoading} isAuthed={!!user}></ProtectedRoute>
+        <ProtectedRoute path="/settings" isLoading={isLoading} isAuthed={!!user}><Settings/></ProtectedRoute>
         <ProtectedRoute path="signOut" isLoading={isLoading} isAuthed={!!user}></ProtectedRoute>
         <ProtectedRoute path="/createProject" isLoading={isLoading} isAuthed={!!user}></ProtectedRoute>
 
