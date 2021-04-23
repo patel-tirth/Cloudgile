d38f296f37c391095da312ce81a0a0cae2da277d
Akshant Jain <akshant.jain2@gmail.com>
2021-04-22 19:21:43 -0500

    big fixed.
    Site is now live at
    https://cloudgile.web.app

--- Code/cloudgile/src/firebase.js
+++ Code/cloudgile/src/firebase.js
@@ -3,14 +3,14 @@ import * as firebaseui from 'firebaseui'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDING_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  apiKey: "AIzaSyBL2AUZRy6mi_m95GT4kV7L4fsT7Axg-Zo",
+  authDomain: "cloudgile.firebaseapp.com",
+  databaseURL: "https://cloudgile-default-rtdb.firebaseio.com",
+  projectId: "cloudgile",
+  storageBucket: "cloudgile.appspot.com",
+  messagingSenderId: "629892471859",
+  appId: "1:629892471859:web:4f732e31d708bcf9925746",
+  measurementId: "G-9B4R9E9LTH"
 };
 
  firebase.initializeApp(firebaseConfig);
